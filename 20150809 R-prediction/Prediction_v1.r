4c81aa48edfa711e4783385fafbfb67391e9a97b
WOBvx <188182427@qq.com>
2015-08-09 18:58:03 +0800

    R - pred

--- 20150809 R-prediction/Prediction_v1.r
+++ 20150809 R-prediction/Prediction_v1.r
@@ -11,25 +11,25 @@ for (i in 1:(l-Num+1)){
 }
 
 
-xulie_train <- xulie[,1:(Num-1)] #取值，除最后 1 位作为预测值
+xulie_train <- xulie[idx,1:(Num-1)] #取值，除最后 1 位作为预测值
 #此处把所有数据作为训练，因为数据少
-xulie_test <- xulie[idx,1:(Num-1)] #随机取一半做测试
+xulie_test <- xulie[-idx,1:(Num-1)] #随机取一半做测试
 xulie_test2 <- xulie[(l-Num+1),2:Num] #取最后一行（真实预测）
-labels <- class.ind(xulie[,Num]) #labels作为结果值
+labels <- class.ind(xulie[,Num]) #labels作为结果值（标靶）
 
-myxulie<-nnet(xulie_train, labels,size=2, rang=0.1,decay=5e-4, maxit=200);
+myxulie<-nnet(xulie_train, labels[idx,],size=2, rang=0.1,decay=5e-4, maxit=200);
 
 ######
 #检测准确率
 
 test.cl <- function(true, pred) {
-true <- max.col(true)
-cres <- max.col(pred)
+true <- max.col(true) #取 true 矩阵每行最大值,true来自标靶
+cres <- max.col(pred) #pred，预测值来自test
 table(true, cres)
 }
-conf<-test.cl(labels[-idx,], predict(myxulie, xulie_test2));
+conf<-test.cl(labels[-idx,], predict(myxulie, xulie_test)); #检测通过现有预测模型预测的准确性
 acc<-sum(diag(conf))/sum(conf);
 cat("The accuracy on the test set is", acc,"\n");
 
 #预测结果
-predict(myxulie, xulie_test2)
+predict(myxulie, xulie_test2) #预测未来值
