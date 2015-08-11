687b20dc544069f851069d6131900ef561afe082
WOBvx <188182427@qq.com>
2015-08-11 18:28:00 +0800

    增加 while 循环，提高猜测概率

--- 20150809 R-prediction/Prediction_v1.r
+++ 20150809 R-prediction/Prediction_v1.r
@@ -1,6 +1,7 @@
 library(nnet)
 #shuzu <- #数据源
 
+
 Num <- 5 #取之范围
 l <- length(shuzu)
 idx = sample(1:(l-Num+1),floor((l-Num+1)/2),replace=FALSE) #随机取一半
@@ -18,12 +19,12 @@ xulie_test2 <- xulie[(l-Num+1),2:Num] #取最后一行（真实预测）
 labels <- class.ind(xulie[,Num]) #labels作为结果值（标靶）
 
 
-#for
+acc=0; #用于while计数
+count=0; #用于while计数
+while(acc<0.4){ #让计算机反复计算，得出「具有一定价值」的模型
 myxulie<-nnet(xulie_train, labels[idx,],size=2, rang=0.1,decay=5e-4, maxit=200);
-
 ######
 #检测准确率
-
 test.cl <- function(true, pred) {
 true <- max.col(true) #取 true 矩阵每行最大值,true来自标靶
 cres <- max.col(pred) #pred，预测值来自test
@@ -31,6 +32,8 @@ table(true, cres)
 }
 conf<-test.cl(labels[-idx,], predict(myxulie, xulie_test)); #检测通过现有预测模型预测的准确性
 acc<-sum(diag(conf))/sum(conf);
+if(count>20){break}; #当重复次数超过20时候，就跳出
+}; #while结束
 cat("The accuracy on the test set is", acc,"\n");
 
 #预测结果
