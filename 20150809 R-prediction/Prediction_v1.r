6874557732a6bf2fbb20ba05dbcb5937a51dc5d7
WOBvx <188182427@qq.com>
2015-08-12 17:51:50 +0800

    Prediction 加入排序，使最末break出来的是所有计算结果的最大值。

--- 20150809 R-prediction/Prediction_v1.r
+++ 20150809 R-prediction/Prediction_v1.r
@@ -20,6 +20,8 @@ labels <- class.ind(xulie[,Num]) #labels作为结果值（标靶）
 
 
 acc=0; #用于while计数
+accMax=0; #用于对比取值
+myxulieMax=0; #用于对比取值
 count=0; #用于while计数
 while(acc<0.4){ #让计算机反复计算，得出「具有一定价值」的模型
 myxulie<-nnet(xulie_train, labels[idx,],size=2, rang=0.1,decay=5e-4, maxit=200);
@@ -32,10 +34,11 @@ table(true, cres)
 }
 conf<-test.cl(labels[-idx,], predict(myxulie, xulie_test)); #检测通过现有预测模型预测的准确性
 acc<-sum(diag(conf))/sum(conf);
+if(accMax<acc){accMax<-acc;myxulieMax<-myxulie}; #用于得出最大值
 count <- count+1;
 if(count>200){break}; #当重复次数超过20时候，就跳出
 }; #while结束
-cat("The accuracy on the test set is", acc,"\n");
+cat("The accuracy on the test set is", accMax,"\n");
 
 #预测结果
-predict(myxulie, xulie_test2) #预测未来值
+predict(myxulieMax, xulie_test2) #预测未来值
