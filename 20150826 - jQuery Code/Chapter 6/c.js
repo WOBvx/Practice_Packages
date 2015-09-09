1c17f4031ee5e49ef2e6027c73204400e1b5164e
WOBvx <188182427@qq.com>
2015-09-09 15:58:05 +0800

    Train-refresh

--- 20150826 - jQuery Code/Chapter 6/c.js
+++ 20150826 - jQuery Code/Chapter 6/c.js
@@ -41,4 +41,12 @@ $.each(entries, function() {
   html += '</div>';
 });
 
+$.each(entries, function(){
+  html += '<div class="entry">';
+  html += '<h3 class="term">' + this.term +'</h3>';
+  html += '<div class ="part">' + this.part +"</div>";
+  html += '<div class="definition">' + this.definition + "</div>";
+  html += '</div>';
+});
+
 $('#dictionary').html(html);
